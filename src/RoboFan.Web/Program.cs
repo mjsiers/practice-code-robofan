3d73e5c551b3292fef58abb69bfb01ad5737424a
Michael Siers <msiers@saturnsys.com>
2019-01-29 15:28:37 -0600

    Updating to get Serilog working.  Not done yet.

--- src/RoboFan.Web/Program.cs
+++ src/RoboFan.Web/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace RoboFan.Web
 {
@@ -14,11 +16,19 @@ namespace RoboFan.Web
   {
     public static void Main(string[] args)
     {
+      Log.Logger = new LoggerConfiguration()
+          .MinimumLevel.Debug()
+          .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+          .Enrich.FromLogContext()
+          .WriteTo.Console()
+          .CreateLogger();
+
       CreateWebHostBuilder(args).Build().Run();
     }
 
     public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
         WebHost.CreateDefaultBuilder(args)
-            .UseStartup<Startup>();
+            .UseStartup<Startup>()
+            .UseSerilog();
   }
 }
