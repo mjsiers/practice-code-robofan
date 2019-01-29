cb1e44cc032a1befe8c4bb2f94e0940b0ede96bb
Michael Siers <msiers@saturnsys.com>
2019-01-28 22:18:27 -0600

    Stubbed in new generator unit test.

---
+++ src/RoboFan.UnitTest/Generators/RoboFanTeamRankingsGeneratorTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+using RoboFan.Data.Mock.Generators;
+using System;
+
+namespace RoboFan.UnitTest.Generators
+{
+  public class RoboFanTeamRankingsGeneratorTest
+  {
+    [Fact]
+    public void VerifyRoboFanTeamRankingsGenerator()
+    {
+    }
+  }
+}
