4a443ec2906723b7e2fce352501b271104478845
Michael Siers <msiers@saturnsys.com>
2019-01-29 22:47:17 -0600

    Refactored shared code into private utility function.

--- src/RoboFan.Data.Mock/Generators/RoboFanImageGenerator.cs
+++ src/RoboFan.Data.Mock/Generators/RoboFanImageGenerator.cs
@@ -9,25 +9,15 @@ namespace RoboFan.Data.Mock.Generators
   {
     public static async Task<RoboFanImage> GenerateAsync(int robofanid, bool fetchImage = true)
     {
-      const int imgwidth = 80;
-      const int imgheight = 80;
-      RoboFanImage fanimage = new RoboFanImage();
-
-      fanimage.Id = robofanid;
-      fanimage.GuidId = Guid.NewGuid();
-      fanimage.Width = imgwidth;
-      fanimage.Height = imgheight;
-      fanimage.ContentType = "image/png";
-      fanimage.RoboFanId = robofanid;
-
-      // ensure we are requested to fetch the robot image
+      // create and ensure we are requested to fetch the robot image
+      RoboFanImage fanimage = Create(robofanid);
       if (fetchImage)
       {
         // create web client to download robot images with
         using (var client = new WebClient())
         {
           // generate a random robot image using the robohash website and download the image data
-          var imageurl = string.Format("https://robohash.org/robots/{0}?size={1}x{2}", fanimage.GuidId, imgwidth, imgheight);
+          var imageurl = string.Format("https://robohash.org/robots/{0}?size={1}x{2}", fanimage.GuidId, fanimage.Width, fanimage.Height);
           fanimage.ImageData = await client.DownloadDataTaskAsync(imageurl);
         }
       }
@@ -36,29 +26,36 @@ namespace RoboFan.Data.Mock.Generators
     }
 
     public static RoboFanImage Generate(int robofanid, bool fetchImage = true)
-    { 
-      const int imgwidth = 80;
-      const int imgheight = 80;
-      RoboFanImage fanimage = new RoboFanImage();
-
-      fanimage.Id = robofanid;
-      fanimage.GuidId = Guid.NewGuid();
-      fanimage.Width = imgwidth;
-      fanimage.Height = imgheight;
-      fanimage.ContentType = "image/png";
-      fanimage.RoboFanId = robofanid;
-
-      // ensure we are requested to fetch the robot image
+    {
+      // create and ensure we are requested to fetch the robot image
+      RoboFanImage fanimage = Create(robofanid);
       if (fetchImage)
       {
         // create web client to download robot images with
         using (var client = new WebClient())
         {
           // generate a random robot image using the robohash website and download the image data
-          var imageurl = string.Format("https://robohash.org/robots/{0}?size={1}x{2}", fanimage.GuidId, imgwidth, imgheight);
+          var imageurl = string.Format("https://robohash.org/robots/{0}?size={1}x{2}", fanimage.GuidId, fanimage.Width, fanimage.Height);
           fanimage.ImageData = client.DownloadData(imageurl);
         }
       }
+
+      return fanimage;
+    }
+
+    private static RoboFanImage Create(int robofanid)
+    {
+      const int imgwidth = 80;
+      const int imgheight = 80;
+
+      // create and initialize the values
+      RoboFanImage fanimage = new RoboFanImage();
+      fanimage.Id = robofanid;
+      fanimage.GuidId = Guid.NewGuid();
+      fanimage.Width = imgwidth;
+      fanimage.Height = imgheight;
+      fanimage.ContentType = "image/png";
+      fanimage.RoboFanId = robofanid;
 
       return fanimage;
     }
